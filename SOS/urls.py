146b875b9a99f01cbf6e1dce21872bc5c578c643
Bicky Yadav <yadavbicky99@gmail.com>
2023-11-29 16:43:40 +0545

    login fixed

--- SOS/urls.py
+++ SOS/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from . import views
+from account import views
 
 urlpatterns = [
-    
+    path('', views.login, name='login'),
     path('admin/', admin.site.urls),
     path('account/',include('account.urls')),
 ]
--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 app_name ='account'
 urlpatterns =[
-path('login/', views.login, name='login'),
 path('logout/',views.logout, name='logout'),
 path('myAccount/',views.myAccount, name='myAccount'),
 path('admindashboard/',views.admindashboard, name='admindashboard'),
